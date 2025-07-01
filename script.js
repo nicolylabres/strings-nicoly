02b6addbcf4234bc02e9232614504d4ef1f64dcd
nicolylabres <nicoly.labres.souza@escola.pr.gov.br>
2025-07-01 17:51:41 +0000

    aula 6

--- script.js
+++ script.js
@@ -12,7 +12,19 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
-     let frequencias = {};
+     const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
         for (let j of palavras) {
@@ -21,8 +33,5 @@ function processaTexto(texto) {
             }
         }
     }
-
-    console.log(frequencias);
-
-    return palavras;
+    return frequencias;
 }
